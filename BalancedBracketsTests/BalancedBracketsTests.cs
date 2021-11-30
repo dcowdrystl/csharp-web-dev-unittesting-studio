cebbf8b7b7b1dfd31932bab182231a30a6b7e3a0
Dan Cowdry <medtc411@gmail.com>
2021-11-29 19:16:17 -0800

    first 3 tests

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BalancedBracketsNS;
+using System;
 
 namespace BalancedBracketsTests
 {
@@ -13,5 +14,31 @@ namespace BalancedBracketsTests
         {
             Assert.AreEqual(true, true);
         }
+
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
+
+        [TestMethod]
+        public void OnlyBracketsReturnsFalse()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
+
+        BalancedBrackets test;
+        [TestInitialize]
+        public void CreateBalancedBracketsObject()
+        {
+            test = new BalancedBrackets();
+        }
+        [TestMethod]
+        public void OnlyBracketsReturnsIsNotNull()
+        {
+           Assert.IsNotNull(test);
+        }
+
     }
 }
